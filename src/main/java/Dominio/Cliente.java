30fbaf43db768dc7294c56d98c88f292c10bc4c6
Unknown <mmalleasantagada@est.frba.utn.edu.ar>
2018-04-19 14:13:29 -0300

    cambios menores en metodos de Cliente

--- src/main/java/Dominio/Cliente.java
+++ src/main/java/Dominio/Cliente.java
@@ -13,11 +13,10 @@ public class Cliente {
 	private Date fechaAlta;
 	private Categoria categoria;
 	private List<Dispositivo> dispositivos;
-	
-//	private String nombreUsuario;
-//	private String contrasenia;	
-	
-	
+
+	// private String nombreUsuario;
+	// private String contrasenia;
+
 	public Cliente(String nombre, String apellido, TipoDocumento tipoDocumento, Integer nroDocumento,
 			Integer nroTelefono, String domicilio) {
 		super();
@@ -28,34 +27,35 @@ public class Cliente {
 		this.nroTelefono = nroTelefono;
 		this.domicilio = domicilio;
 	}
-	
-//	private void categorizar() {
-//		this.categoria=this.categorias.stream().findAny(categoria->cateegoria.pertenece(this.consumoMensual)).get();
-//	}
 
+	// private void categorizar() {
+	// this.categoria=this.categorias.stream().findAny(categoria->cateegoria.pertenece(this.consumoMensual)).get();
+	// }
 
-	/*Con respecto a estos requerimientos del cliente, para mi, podriamos solamente usar este metodo.
+	/*
+	 * Con respecto a estos requerimientos del cliente, para mi, podriamos solamente
+	 * usar este metodo.
+	 * 
+	 * ● Saber la cantidad de dispositivos encendidos: es la funcion que defini ●
+	 * Saber la cantidad de dispositivos apagados: le haces el size a la lista de
+	 * dispositivos, y le restas los que estan prendidos con el metodo que hice
+	 * abajo
+	 * 
+	 * Igualmente, lo codeo, pero el viernes se lo pregunto porque simplificaria
+	 * bastante el código
+	 * 
+	 */
 
-		● Saber la cantidad de dispositivos encendidos: es la funcion que defini
-		● Saber la cantidad de dispositivos apagados: le haces el size a la lista de dispositivos, y le restas los que estan prendidos con el metodo que hice abajo
-
-		Igualmente, lo codeo, pero el viernes se lo pregunto porque simplificaria bastante el código
-
-	*/
-
-	private Long cuantosDispositivosEstanEncendidos(List<Dispositivo> dispositivosList){
-		Long cantidadDeEncendidos = dispositivosList.stream().filter(dispositivo -> dispositivo.getStatus()).count();
-		return cantidadDeEncendidos;
+	private Long cuantosDispositivosEstanEncendidos(List<Dispositivo> dispositivosList) {
+		return dispositivosList.stream().filter(dispositivo -> dispositivo.getStatus()).count();
 	}
 
-	private Long cuantosDispositivosEstanApagados(List<Dispositivo> dispositivoList){
-		Long cantidadApagados = this.dispositivos.size() - this.cuantosDispositivosEstanEncendidos(dispositivoList);
-		return cantidadApagados;
+	private Long cuantosDispositivosEstanApagados(List<Dispositivo> dispositivoList) {
+		return this.dispositivos.size() - this.cuantosDispositivosEstanEncendidos(dispositivoList);
 	}
 
-	public int cantidadDispositivos(List<Dispositivo> dispositivosList ) {
+	public int cantidadDispositivos(List<Dispositivo> dispositivosList) {
 		return dispositivosList.size();
 	}
-
 
 }
