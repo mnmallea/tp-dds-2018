e105bcb6755b7fc20f267b5a468234a4effcafdc
francoarcu <francocuri.fc@gmail.com>
2018-04-21 01:37:43 -0300

    Definición de categorías y montoEstimadoAPagar

--- src/main/java/Dominio/Categoria.java
+++ src/main/java/Dominio/Categoria.java
@@ -1,10 +1,16 @@
 package Dominio;
 
-public abstract class Categoria {
-	public Float cargoVariable;
+public class Categoria {
 	public Float cargoFijo;
-		
-	public Float estimarFactura(Float unConsumo) {
+	public Float cargoVariable;
+	
+	public Categoria(Float cargoFijo, Float cargoVariable) {
+		super();
+		this.cargoFijo = cargoFijo;
+		this.cargoVariable = cargoVariable;
+	}
+
+	public Float facturaEstimada(Float unConsumo) {
 		return this.cargoFijo + unConsumo*this.cargoVariable;
 	}
 
--- src/main/java/Dominio/Cliente.java
+++ src/main/java/Dominio/Cliente.java
@@ -5,6 +5,9 @@ import java.util.List;
 import org.joda.time.LocalDate;
 
 public class Cliente {
+	public Float consumoMesAnterior;
+	public Float consumoUltimoMes;
+	
 	private String nombre;
 	private String apellido;
 	private TipoDocumento tipoDocumento;
@@ -14,12 +17,23 @@ public class Cliente {
 	private LocalDate fechaAlta;
 	private Categoria categoria;
 	private List<Dispositivo> dispositivos;
-
 	// private String nombreUsuario;
 	// private String contrasenia;
+	
+	//Tipos de categoria que puede adoptar un cliente
+	static final Categoria r1 = new Categoria(18.76f, 0.644f);
+	static final Categoria r2 = new Categoria(35.32f, 0.644f);
+	static final Categoria r3 = new Categoria(60.71f, 0.681f);
+	static final Categoria r4 = new Categoria(71.74f, 0.738f);
+	static final Categoria r5 = new Categoria(110.38f, 0.794f);
+	static final Categoria r6 = new Categoria(220.75f, 0.832f);
+	static final Categoria r7 = new Categoria(443.59f, 0.851f);
+	static final Categoria r8 = new Categoria(545.96f, 0.851f);
+	static final Categoria r9 = new Categoria(887.19f, 0.851f);
+	//-----------------------------------------------
 
 	public Cliente(String nombre, String apellido, TipoDocumento tipoDocumento, Integer nroDocumento,
-			Integer nroTelefono, String domicilio) {
+			Integer nroTelefono, String domicilio, Categoria categoria) {
 		super();
 		this.nombre = nombre;
 		this.apellido = apellido;
@@ -27,23 +41,33 @@ public class Cliente {
 		this.nroDocumento = nroDocumento;
 		this.nroTelefono = nroTelefono;
 		this.domicilio = domicilio;
+		this.categoria = categoria;
 	}
 
 	// private void categorizar() {
 	// this.categoria=this.categorias.stream().findAny(categoria->cateegoria.pertenece(this.consumoMensual)).get();
 	// }
+	
+	/* private void categorizar(Float consumoMesAnterior) { 
+	  // Determina la categoria a la que pertenece el cliente segun el consumo del mes anterior
+ 
+	 * Para esta entrega podriamos asumir que la categoria viene determinada
+	 * cuando se instancia al cliente
+	 * (Agrego -categoria- al constructor)
+
+	} */
 
 	/*
 	 * Con respecto a estos requerimientos del cliente, para mi, podriamos solamente
 	 * usar este metodo.
 	 * 
-	 * ● Saber la cantidad de dispositivos encendidos: es la funcion que defini ●
+	 * â—� Saber la cantidad de dispositivos encendidos: es la funcion que defini â—�
 	 * Saber la cantidad de dispositivos apagados: le haces el size a la lista de
 	 * dispositivos, y le restas los que estan prendidos con el metodo que hice
 	 * abajo
 	 * 
 	 * Igualmente, lo codeo, pero el viernes se lo pregunto porque simplificaria
-	 * bastante el código
+	 * bastante el cÃ³digo
 	 * 
 	 */
 
@@ -57,6 +81,10 @@ public class Cliente {
 
 	public int cantidadDispositivos(List<Dispositivo> dispositivosList) {
 		return dispositivosList.size();
+	}
+	
+	public Float montoEstimadoAPagar() {
+		return categoria.facturaEstimada(consumoUltimoMes);
 	}
 
 }
