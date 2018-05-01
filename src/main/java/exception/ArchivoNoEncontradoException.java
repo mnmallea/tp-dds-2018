b83727ed24d18a19975ed6d6e8e6400318dce7c5
efahnle <eric.fahnle@gmail.com>
2018-05-01 16:35:38 -0300

    fixed #11

---
+++ src/main/java/exception/ArchivoNoEncontradoException.java
@@ -0,0 +1,6 @@
+package exception;
+
+public class ArchivoNoEncontradoException extends RuntimeException{
+	public ArchivoNoEncontradoException (String s) {
+	}
+}
---
+++ src/main/java/exception/ParserException.java
@@ -0,0 +1,8 @@
+package exception;
+
+public class ParserException extends RuntimeException{
+	public ParserException(String s) {
+		
+	}
+	
+}
