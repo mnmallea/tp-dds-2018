557ef63fff0fc94fb8e015bc00bebc95755d9935
mnmallea <mmalleasantagada@est.frba.utn.edu.ar>
2018-10-24 00:10:02 -0300

    Guardando a belen

--- src/main/java/Bootstrap.java
+++ src/main/java/Bootstrap.java
@@ -1,9 +1,12 @@
+import dominio.TipoUsuario;
 import dominio.Transformador;
+import dominio.Usuario;
 import org.uqbarproject.jpa.java8.extras.EntityManagerOps;
 import org.uqbarproject.jpa.java8.extras.WithGlobalEntityManager;
 import org.uqbarproject.jpa.java8.extras.transaction.TransactionalOps;
 import puntos.Point;
 import repositorios.RepoTransformadores;
+import repositorios.RepoUsuarios;
 
 import java.util.Arrays;
 import java.util.List;
@@ -22,6 +25,8 @@ public class Bootstrap implements WithGlobalEntityManager, TransactionalOps, Ent
         List<Transformador> transformadores = Arrays.asList(new Transformador(new Point(10, 20)), new Transformador(new Point(-10, 40)), new Transformador(new Point(100, 121.5)));
         withTransaction(() -> {
             RepoTransformadores.instancia.agregarTransformadores(transformadores);
+            RepoUsuarios.instancia.guardar(new Usuario(1234L,"belen", TipoUsuario.ADMINISTRADOR ));
+
         });
     }
 }
--- src/main/java/Runner.java
+++ src/main/java/Runner.java
@@ -1,8 +1,4 @@
 import org.quartz.SchedulerException;
-
-import dominio.TipoUsuario;
-import dominio.Usuario;
-import repositorios.RepoUsuarios;
 import simplex.SimplexJob;
 import spark.Spark;
 import spark.debug.DebugScreen;
@@ -11,7 +7,6 @@ public class Runner {
     public static void main(String[] args) throws SchedulerException {
         SimplexJob.init();
         //TODO HACER PK LONG DE REPOUSUARIOS
-        RepoUsuarios.instancia.guardar(new Usuario(1234L,"belen",TipoUsuario.ADMINISTRADOR ));
         Spark.ipAddress("0.0.0.0");
         Spark.port(9000);
         DebugScreen.enableDebugScreen();
