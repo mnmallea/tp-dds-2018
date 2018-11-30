60805360331ae23035ad84ec78f1bb5d83274653
efahnle <eric.fahnle@gmail.com>
2018-11-30 20:55:45 -0300

    todo volvio a la normalidad con simplex andando

--- src/main/java/app/Routes.java
+++ src/main/java/app/Routes.java
@@ -20,64 +20,65 @@ import handlebarsUtils.BooleanHelper;
 import handlebarsUtils.EqualityHelper;
 import handlebarsUtils.HandlebarsTemplateEngineBuilder;
 import handlebarsUtils.IsNumberHelper;
-import repositorios.RepoTransformadores;
 import spark.ModelAndView;
 import spark.template.handlebars.HandlebarsTemplateEngine;
 
 public class Routes {
-    public static void configure() {
-        HandlebarsTemplateEngine engine = HandlebarsTemplateEngineBuilder
-                .create()
-                .withDefaultHelpers()
-                .withHelper("isTrue", BooleanHelper.isTrue)
-                .withHelper("isNumber", new IsNumberHelper())
-                .withHelper("equals", new EqualityHelper())
-                .build();
+	public static void configure() {
+		HandlebarsTemplateEngine engine = HandlebarsTemplateEngineBuilder.create().withDefaultHelpers()
+				.withHelper("isTrue", BooleanHelper.isTrue).withHelper("isNumber", new IsNumberHelper())
+				.withHelper("equals", new EqualityHelper()).build();
 
-        staticFiles.location("/public");
+		staticFiles.location("/public");
 
-        exception(UnauthorizedException.class, ((exception, request, response) -> {
-            ModelAndView modelAndView = new ModelAndView(null, "errors/unauthorized.hbs");
-            String render = engine.render(modelAndView);
-            response.body(render);
-            response.status(401);
-        }));
+		exception(UnauthorizedException.class, ((exception, request, response) -> {
+			ModelAndView modelAndView = new ModelAndView(null, "errors/unauthorized.hbs");
+			String render = engine.render(modelAndView);
+			response.body(render);
+			response.status(401);
+		}));
 
-        before("/", LoginValidator::validate);
-        get("/", HomeController::show, engine);
-        get("/login", LoginController::show, engine);
-        post("/login", LoginController::login);
-        get("/logout", LoginValidator::removeAuthenticatedUser);
-        before("/reportes/*", LoginValidator::validateAdmin);
-        get("/reportes/transformadores/consumos", TransformadoresController::show, engine);
-        get("/reportes/usuarios", HogaresController::seleccionarDispositivos, engine);
-        get("/reportes/usuarios/:id/promedios", DispositivosController::showConsumos, engine);
-        get("/reportes/hogares/consumos", HogaresController::consumosPorPeriodo, engine);
+		before("/", LoginValidator::validate);
+		get("/", HomeController::show, engine);
+		get("/login", LoginController::show, engine);
+		post("/login", LoginController::login);
+		get("/logout", LoginValidator::removeAuthenticatedUser);
+		before("/reportes/*", LoginValidator::validateAdmin);
+		get("/reportes/transformadores/consumos", TransformadoresController::show, engine);
+		get("/reportes/usuarios", HogaresController::seleccionarDispositivos, engine);
+		get("/reportes/usuarios/:id/promedios", DispositivosController::showConsumos, engine);
+		get("/reportes/hogares/consumos", HogaresController::consumosPorPeriodo, engine);
 
+		path("/administrador", () -> {
+			before("", LoginValidator::validateAdmin);
+			before("/*", LoginValidator::validateAdmin);
+			get("", HomeController::show, engine);
+			get("/hogares", HogaresController::show, engine);
+		});
 
-        path("/administrador", () -> {
-            before("", LoginValidator::validateAdmin);
-            before("/*", LoginValidator::validateAdmin);
-            get("", HomeController::show, engine);
-            get("/hogares", HogaresController::show, engine);
-        });
+		path("/clientes/:id", () -> {
+			get("/hogar", ((request, response) -> {
+				Cliente cliente = (Cliente) LoginValidator.getUsuario(request);
+				return ClienteController.home(cliente);
+			}), engine);
+			get("/consumos", ((request, response) -> {
+				Cliente cliente = (Cliente) LoginValidator.getUsuario(request);
+				return ClienteController.consumos(request, cliente);
+			}), engine);
+			get("/consumos?", ((request, response) -> {
+				Cliente cliente = (Cliente) LoginValidator.getUsuario(request);
+				return ClienteController.consumos(request, cliente);
+			}), engine);
 
-        path("/clientes/:id", () -> {
-        	get("/hogar", ((request, response) -> {
-                Cliente cliente = (Cliente) LoginValidator.getUsuario(request);
-                return ClienteController.home(cliente);
-            }), engine);
-             get("/consumos", ((request, response) -> {
-                Cliente cliente = (Cliente) LoginValidator.getUsuario(request);
-                return ClienteController.consumos(request, cliente);
-             }), engine);
-              get("/consumos?", ((request, response) -> {
-                 Cliente cliente = (Cliente) LoginValidator.getUsuario(request);
-                 return ClienteController.consumos(request, cliente);
-             }), engine);
-        });
+			get("/simplex", ((request, response) -> {
+				Cliente cliente = (Cliente) LoginValidator.getUsuario(request);
+				return ClienteController.simplex(cliente);
+			}), engine);
+		});
 
-        //get("/administradores/:id/*", AdministradorController::show);
-        //get("/clientes/:id/*", (request, response) -> "<html> <body> <h1>" + RepoTransformadores.instancia.getTransformadores() + "</h1> </body> </html>");
-    }
+		// get("/administradores/:id/*", AdministradorController::show);
+		// get("/clientes/:id/*", (request, response) -> "<html> <body> <h1>" +
+		// RepoTransformadores.instancia.getTransformadores() + "</h1> </body>
+		// </html>");
+	}
 }
--- src/main/java/controllers/ClienteController.java
+++ src/main/java/controllers/ClienteController.java
@@ -1,15 +1,17 @@
 package controllers;
 
+import java.time.LocalDateTime;
+import java.util.HashMap;
+import java.util.List;
+
 import dominio.Cliente;
 import dominio.dispositivos.Dispositivo;
 import dominio.dispositivos.DispositivoInteligente;
 import dominio.dispositivos.Periodo;
+import simplex.OptimizadorHoras;
+import simplex.SolucionSimplex;
 import spark.ModelAndView;
 import spark.Request;
-
-import java.time.LocalDateTime;
-import java.util.HashMap;
-import java.util.List;
 
 public class ClienteController {
     public static ModelAndView home(Cliente cliente) {
@@ -43,4 +45,17 @@ public class ClienteController {
 
         return new ModelAndView(model, "cliente/consumos.hbs");
     }
+    
+    public static ModelAndView simplex(Cliente cliente){
+    	HashMap model = new HashMap();
+    	List<Dispositivo> dispositivos = cliente.getDispositivos();
+    	OptimizadorHoras optimizador = new OptimizadorHoras();
+    	List<SolucionSimplex> solucionesSimplex = optimizador.optimizarHorasUso(dispositivos);
+    	String persona = cliente.getNombre();
+    	model.put("persona", persona);
+    	model.put("dispositivos", dispositivos);
+    	model.put("solucionesSimplex", solucionesSimplex);
+    	return new ModelAndView(model, "cliente/simplex.hbs");
+    }
+
 }
